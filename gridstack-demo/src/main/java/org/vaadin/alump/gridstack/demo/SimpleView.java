dbfb361b082f6bb64087aef36f8c7b6acab7fd38
Sami Viitanen <sami.viitanen@vaadin.com>
2017-03-14 20:31:23 -0700

    O noes, 0.4.0 demo doesn't work on remote server

--- gridstack-demo/src/main/java/org/vaadin/alump/gridstack/demo/SimpleView.java
+++ gridstack-demo/src/main/java/org/vaadin/alump/gridstack/demo/SimpleView.java
@@ -3,6 +3,7 @@ package org.vaadin.alump.gridstack.demo;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 import org.vaadin.alump.gridstack.GridStackLayout;
 import org.vaadin.alump.scaleimage.ScaleImage;
 
@@ -44,7 +45,7 @@ public class SimpleView extends AbstractView {
         initialize();
     }
 
-    private Component createChild() {
+    private Component createImageChild() {
         String resourceName;
         switch (childCounter.incrementAndGet() % 4) {
             case 1:
@@ -67,13 +68,33 @@ public class SimpleView extends AbstractView {
         return image;
     }
 
+    private Component createTextChild() {
+        CssLayout wrapper = new CssLayout();
+        wrapper.addStyleName("simple-promo");
+        wrapper.setSizeFull();
+        VerticalLayout layout = new VerticalLayout();
+        layout.setWidth(100, Unit.PERCENTAGE);
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        Label header = new Label("GridStack add-on");
+        header.setWidth(100, Unit.PERCENTAGE);
+        header.addStyleName("header");
+        layout.addComponent(header);
+        Label desc = new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+        desc.addStyleName("desc");
+        desc.setWidth(100, Unit.PERCENTAGE);
+        layout.addComponent(desc);
+        wrapper.addComponent(layout);
+        return wrapper;
+    }
+
     private void initialize() {
         gridStack.removeAllComponents();
-        gridStack.addComponent(createChild(), 0, 0, 2, 1, false);
-        gridStack.addComponent(createChild(), 2, 0, 1, 2, false);
-        gridStack.addComponent(createChild(), 0, 1, 1, 1, false);
-        gridStack.addComponent(createChild(), 1, 1, 1, 1, false);
-        gridStack.addComponent(createChild(), 0, 2, 3, 1, false);
+        gridStack.addComponent(createImageChild(), 0, 0, 2, 1, false);
+        gridStack.addComponent(createImageChild(), 2, 0, 1, 2, false);
+        gridStack.addComponent(createImageChild(), 0, 1, 1, 1, false);
+        gridStack.addComponent(createImageChild(), 1, 1, 1, 1, false);
+        gridStack.addComponent(createTextChild(), 0, 2, 3, 1, false);
         gridStack.iterator().forEachRemaining(c -> gridStack.setWrapperScrolling(c, false));
     }
 
